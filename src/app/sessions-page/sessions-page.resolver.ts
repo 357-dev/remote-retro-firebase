775db85c3a7ce677e3ddf0968f2ce37bacf59667
Dgo <nicolaides.d@live.co.uk>
2020-04-12 18:52:30 +0300

    import OCD

--- src/app/sessions-page/sessions-page.resolver.ts
+++ src/app/sessions-page/sessions-page.resolver.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@angular/core';
-import { SessionsPage } from './models/sessions-page.model';
 import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { AngularFireDatabase } from '@angular/fire/database';
 import { Observable, of } from 'rxjs';
-import { map } from 'rxjs/operators'
+import { map } from 'rxjs/operators';
 import { Session } from '../common/session.model';
+import { SessionsPage } from './models/sessions-page.model';
 
 @Injectable({
   providedIn: 'root'
@@ -29,16 +29,3 @@ export class SessionsPageResolver implements Resolve<SessionsPage> {
     return of(model);
   }
 }
-
-/*
-
-
-[ {
-    "payload": {
-      "title": "Retro 1"
-    },
-    "type": "value",
-    "prevKey": null,
-    "key": "-M4ZgvRNdKNd5O9J6LTF" }, { "payload": { "created": "", "title": "asdads" }, "type": "value", "prevKey": "-M4ZgvRNdKNd5O9J6LTF", "key": "-M4Zi2BBXJ0oRYSfsnun" }, { "payload": { "created": "10th Apr 2020", "title": "aaaa" }, "type": "value", "prevKey": "-M4Zi2BBXJ0oRYSfsnun", "key": "-M4ZiR2bsXqklurNtuda" } ]
-
-*/
